0ad0b24a449b46aac30ee69afb4422f820aba349
Daniel Kraemer <dan.kraemer1@gmail.com>
2020-04-14 22:00:30 -0300

    Add Chapter 2 tests

---
+++ fwpd/src/fwpd/core.clj
@@ -0,0 +1,28 @@
+(ns fwpd.core)
+
+(def filename "suspects.csv")
+
+(slurp filename)
+
+(def vamp-keys [:name :glitter-index])
+
+(defn str->int
+  [str]
+  (Integer. str))
+
+(def conversions {:name          identity
+                  :glitter-index str->int})
+
+(defn convert
+  [vamp-key value]
+  ((get conversions vamp-key) value))
+
+(convert :glitter-index "3")
+
+(defn parse
+  "Convert a CSV into rows of columns"
+  [string]
+  (map #(clojure.string/split % #",")
+       (clojure.string/split string #"\n")))
+
+(parse (slurp filename))
