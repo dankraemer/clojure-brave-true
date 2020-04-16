d453d6e9c2b2e342ed448591b7aa4d5fc09e933d
Daniel Kraemer <dan.kraemer1@gmail.com>
2020-04-15 22:04:18 -0300

    Use map, reduce and filter

--- fwpd/src/fwpd/core.clj
+++ fwpd/src/fwpd/core.clj
@@ -26,3 +26,22 @@
        (clojure.string/split string #"\n")))
 
 (parse (slurp filename))
+
+(defn mapify
+  "Return a seq of maps like {:name \"Eduard Cullen\" :glitter-index 10}"
+  [rows]
+  (map (fn [unmapped-row]
+         (reduce (fn [row-map [vamp-key value]]
+                   (assoc row-map vamp-key (convert vamp-key value)))
+                 {}
+                 (map vector vamp-keys unmapped-row)))
+       rows))
+
+(first (mapify (parse (slurp filename))))
+
+(defn glitter-filter
+  [minimum-glitter records]
+  (filter #(>= (:glitter-index %) minimum-glitter) records))
+
+(glitter-filter 3 (mapify (parse (slurp filename))))
+
