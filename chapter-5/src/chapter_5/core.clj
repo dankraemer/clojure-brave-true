b2607fca95a437d2ff33377da795dd8430f2c630
Daniel Kraemer <dan.kraemer1@gmail.com>
2020-04-22 15:31:11 -0300

    Pure functions have referential transparency

--- chapter-5/src/chapter_5/core.clj
+++ chapter-5/src/chapter_5/core.clj
@@ -1,13 +1,42 @@
 (ns chapter-5.core)
 
-; Functional Programming
+;; Functional Programming
 
-; Pure functions: Referential Transparency
+;; Pure functions: Referential Transparency
 
 (+ 1 2)
 
+; Referential transparency
 (defn wisdom
   [words]
   (str words ", Daniel-san"))
 
 (wisdom "Always bathe on Fridays")
+
+(defn analisys
+  [text]
+  (str "Character count: " (count text)))
+
+; Not Referential transparency
+(defn year-end-evaluation
+  []
+  (if (> (rand) 0.5)
+    "You get a raise"
+    "Better luck next year"))
+
+(year-end-evaluation)
+
+(defn analyze-file
+  [filename]
+  (analisys (slurp filename)))
+
+
+
+
+
+
+
+
+
+
+
